8ce621e416826405bff11b2abb387e084132e8bc
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-09-01 11:31:09 +0600

    Student interface and modal created

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,41 @@
+import { Types } from 'mongoose'
+
+export type TUserName = {
+  firstName: string
+  middleName?: string
+  lasTName: string
+}
+export type TGuardian = {
+  fatherName: string
+  fatherContactNo: string
+  fatherOccupation: string
+  motherName: string
+  motherContactNo: string
+  motherOccupation: string
+}
+
+export type TLocalGuardian = {
+  name: string
+  occupation: string
+  contactNo: string
+  address: string
+}
+
+export type TStudent = {
+  id: string
+  user: Types.ObjectId
+  password: string
+  name: TUserName
+  gender: 'male' | 'female' | 'other'
+  dateOfBirth: string
+  email: string
+  contactNo: string
+  emergencyContactNo: string
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-'
+  presentAddress: string
+  permanentAddress: string
+  guardian: TGuardian
+  localGuardian: TLocalGuardian
+  profileImg?: string
+  isDeleted: boolean
+}
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,11 +1,22 @@
+import config from '../../config'
+import { TStudent } from '../student/student.interface'
 import { TUser } from './user.interface'
 import { UserModel } from './user.model'
 
-const createUserIntoDB = async (userData: TUser) => {
-  const result = await UserModel.create(userData)
+const createStudentIntoDB = async (password: string, studentData: TStudent) => {
+  const userData: Partial<TUser> = {}
+
+  userData.password = password || (config.default_pass as string)
+  userData.role = 'student'
+  userData.id = '2030100001'
+  const result = await UserModel.create(studentData)
+  if (Object.keys(result).length) {
+    studentData.id = result.id
+    studentData.user = result._id
+  }
   return result
 }
 
 export const UserServices = {
-  createUserIntoDB,
+  createStudentIntoDB,
 }
